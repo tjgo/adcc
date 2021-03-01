6600684b9d6c3e4a40178db0a7f3a3cb223c3a13
fangzhao <995678025@qq.com>
2021-03-01 21:29:44 +0800

    fix bug

--- app/src/main/java/com/example/androiddevchallenge/PuppyItem.kt
+++ app/src/main/java/com/example/androiddevchallenge/PuppyItem.kt
@@ -6,11 +6,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
